c8e95ad6c1bfa0c209873a7262f09a7fc2437689
Jack Pflug <jack.pflug4@gmail.com>
2020-06-29 02:18:51 -0400

    updates on the repos

---
+++ ProjectZero/StoreApp/StoreApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using BuisnessLogic.Library.Repos;
+using DataAccess.Library.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using DataAccess.Library.Repos;
+using System.Net.Http.Headers;
+
+namespace StoreApp
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            using (var unitofWork = new UnitOfWork(new projectZeroContext()))
+            {
+
+                unitofWork.Customers.Add(new DataAccess.Library.Entities.Customer { FirstName = "Jack", LastName = "Pflug" });
+                unitofWork.Complete(); 
+
+
+
+            }
+
+
+
+    }
+    }
+}
+
+
